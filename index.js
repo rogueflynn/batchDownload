05e5e4d1448f87cab5c7d0bfa1aa204e36ba02c3
rogueflynn <vgvgonzalez8@gmail.com>
2018-01-13 15:34:28 -0500

    Basic Batch Downloading Script

--- index.js
+++ index.js
@@ -6,10 +6,27 @@ const wget = require('wget-improved');
 const stat = util.promisify(fs.stat);
 const mkdir = util.promisify(fs.mkdir);
 const dirPath = Path.join(__dirname, "/tmp");
+const eventEmitter = require("events").EventEmitter;
+const emitter = new eventEmitter();
 const queue = [];
+let url = "";
+let ep = "";
+let index = 0;
 
-//query.push("<URL>");  //enqueue urls somehow (web scraping or pattern recognition)
-//query.push("<URL>");  //enqueue element
+for(let i = 1; i < 49; i++) {
+    /*
+    //If link has 0 based numbering
+    if(i < 10) {
+        ep = "0" + i;
+    } else {
+        ep = i;
+    }
+    */
+    ep = i;
+    url = `<URL> + ${ep}`;
+    queue.push(url);
+}
+
 
 const pathError = async () => {
     try {
@@ -32,22 +49,36 @@ const createDirectory = async () => {
 const nodeWget = () => {
     return new Promise((resolve, reject) =>
     {
-        let src = "<URL>";
-        let output = dirPath + "/<Name OF File>";
+        console.log("Queue Count: " + queue.length);
+        index++;
+        let src = queue.shift();
+        let fileName = "/<filename>" + index + ".<extension>";
+        let output = dirPath + fileName;
         let options = {};
         let download = wget.download(src, output, options);
+        let prevPercent;
+
+        console.log("Downloading: "  + fileName);
 
         download.on('error', (err) => {
             reject(err) 
         });
+
         download.on('start', (filesize) => {
             console.log("File Size: " + filesize);
         });
+
         download.on('end', (output) => {
+            console.log(fileName + " finished downloading");
             resolve("completed downloaded"); 
+            emitter.emit("completed");
         });
-        download.on('progress', (progress) => {
-            // code to show progress bar 
+        download.on("progress", (progress) => {
+            let percent = Math.round(progress * 100);
+            if(percent % 20 === 0 && percent !== prevPercent) {
+                prevPercent = percent;
+                console.log("File " + fileName + " is " + percent + "% completed.");
+            }
         });
     });
 };
@@ -66,3 +97,13 @@ if(pathError())  {
 }
 
 DownloadFiles();
+
+emitter.on("completed", () => {
+    if(queue.length !== 0) {
+        console.log("Downloading another file");
+        DownloadFiles();
+    } else {
+        console.log("completed download");
+    }
+});
+
